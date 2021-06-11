8260d74581c4573104a85693033c9a3e33d946a4
Anita Twumasi Ankrah <58734080+kwartemaa@users.noreply.github.com>
2021-06-11 15:52:08 +0000

    Ok

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ const fs = require ('fs');
 // const files = fs.readdirSync('./');
 // console.log(files);
 
-fs.readdir('$', function(err, files){
+fs.readdir('./', function(err, files){
     if(err) console.log('Error', err);
     else console.log('Result', files);
 
--- logger.js
+++ logger.js
@@ -9,6 +9,3 @@ module.exports = log;
 */
 
 // Using the path module
-const path = require ('path');
-let pathObj = path.parse(__filename);
-console.log(pathObj)
